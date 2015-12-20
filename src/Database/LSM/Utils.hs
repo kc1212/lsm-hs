50226cc33becd7ce39a2112415a56d0144df0f82
Aaron Ang <awz.ang@gmail.com>
2015-12-20 23:58:50 +0100

    Map to Btree

--- src/Database/LSM/Utils.hs
+++ src/Database/LSM/Utils.hs
@@ -6,12 +6,13 @@ import qualified BTree as BT
 import qualified Data.Map as Map
 import Pipes
 import System.FilePath ((</>))
-import Control.Monad (unless)
+import Control.Monad (unless, liftM)
 import Control.Exception (throwIO)
 import Control.Monad.State (liftIO, MonadIO)
 import System.Directory (doesFileExist)
 import System.IO.Error (alreadyExistsErrorType, doesNotExistErrorType, mkIOError)
 import System.Random (randomIO)
+import System.IO
 
 import Database.LSM.Types (ImmutableTable, Bs)
 
@@ -54,3 +55,23 @@ io = liftIO
 
 fromMapToProducer :: ImmutableTable -> Producer (BT.BLeaf Bs Bs) IO ()
 fromMapToProducer table = Map.foldlWithKey (\_ k v -> yield (BT.BLeaf k v)) (return ()) table
+
+# TODO: What should the order and size be?
+getTreeIO :: Producer (BT.BLeaf Bs Bs) IO () -> IO (BT.LookupTree Bs Bs)
+getTreeIO producer = do
+    let order = 10
+    let size = 100
+    hSetBuffering stdin LineBuffering
+    tree <- liftM BT.fromByteString (BT.fromOrderedToByteString order size producer)
+    return $ case tree of
+        Left _ -> error "Whut?"
+        Right x -> x
+
+merge :: (BT.LookupTree Bs Bs) -> (BT.LookupTree Bs Bs) -> IO (BT.LookupTree Bs Bs)
+merge t1 t2 = do
+    BT.mergeTrees (\a _ -> return a) 10 fname [t1, t2]
+    eitherTree <- BT.open fname
+    return $ case eitherTree of
+        Left _ -> error "Whut?"
+        Right x -> x
+    where fname = "/tmp/tree.tmp"
