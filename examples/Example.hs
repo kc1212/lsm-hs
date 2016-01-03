6c294a331acdc878c26dca52b3352a61da046238
Aaron Ang <awz.ang@gmail.com>
2016-01-03 17:51:24 +0100

    Cleaner example

--- examples/Example.hs
+++ examples/Example.hs
@@ -10,12 +10,15 @@ import Database.LSM
 main = do
     withLSM def { dbName = "/tmp/ExampleDB" 
                 , errorIfExists = False } $ do
-        add (C.pack "Key 1") (C.pack "Value 1")
-        res1 <- get (C.pack "Key 1")
-        update (C.pack "Key 1") (C.pack "Value 2")
-        res2 <- get (C.pack "Key 1")
-        delete (C.pack "Key 1")
-        res3 <- get (C.pack "Key 1")
+        let key1 = C.pack "Key 1"
+        let val1 = C.pack "Value 1"
+        let val2 = C.pack "Value 2"
+        add key1 val1
+        res1 <- get key1
+        update key1 val2
+        res2 <- get key1
+        delete key1
+        res3 <- get key1
         liftIO $ print (fromJust res1)
         liftIO $ print (fromJust res2)
         liftIO $ print (fromJust res3)
