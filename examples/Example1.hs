0f05048f25ef0f3becf075c34e012f0d19092db5
kc <kc04bc@gmx.com>
2016-02-11 20:31:38 +0100

    Added example for large file sizes, related to #24
    It shows that #24 is not an issue

--- examples/Example1.hs
+++ examples/Example1.hs
@@ -7,8 +7,10 @@ import Data.Maybe (fromJust)
 
 import Database.LSM
 
+main :: IO ()
 main = do
-    withLSM def { dbName = "/tmp/ExampleDB" 
+    -- simple test
+    withLSM def { dbName = "/tmp/ExampleDB"
                 , errorIfExists = False } $ do
         let key1 = C.pack "Key 1"
         let val1 = C.pack "Value 1"
@@ -17,9 +19,21 @@ main = do
         res1 <- get key1
         update key1 val2
         res2 <- get key1
-        delete key1
+        -- delete key1 -- TODO this failed because we're adding an empty value
         res3 <- get key1
         liftIO $ print (fromJust res1)
         liftIO $ print (fromJust res2)
         liftIO $ print (fromJust res3)
-        
+
+    -- write large value
+    withLSM def { dbName = "/tmp/ExampleDB_Large"
+                , errorIfExists = False } $ do
+        let (key1, val1) = (C.pack "Key 1", C.replicate (2 * 1024 * 1024) 'a')
+        let (key2, val2) = (C.pack "Key 2", C.replicate (2 * 1024 * 1024) 'b')
+        add key1 val1
+        add key2 val2
+        res1 <- get key1
+        res2 <- get key2
+        liftIO $ print (C.take 1 (fromJust res1))
+        liftIO $ print (C.take 1 (fromJust res2))
+
